4b9c7ad49d4d03fa370c1663da5bf8b93bb5a56f
Charles Swaleh <charlswall@live.com>
2023-01-28 21:38:24 +0300

    Completed the function to change the color of the parent when the child is clicked

--- src/Child.js
+++ src/Child.js
@@ -2,7 +2,7 @@ import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
 function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+  return <div className="child" style={{ backgroundColor: "#FFF" }} onClick={onChangeColor}/>;
 }
 
 export default Child;
